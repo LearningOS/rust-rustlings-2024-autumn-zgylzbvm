12f942a390d69f823d39437d01e13fd272e13f6b
zgylzbvm <2693943158@qq.com>
2024-10-19 23:35:19 +0800

    solve all tests.rs

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {}
 
@@ -17,9 +17,10 @@ mod tests {
 
     #[test]
     fn test_success() {
-        #[cfg(feature = "pass")]
+        if cfg(feature = "pass"){
         return;
-
+    } else {
         panic!("no cfg set");
     }
+}
 }
