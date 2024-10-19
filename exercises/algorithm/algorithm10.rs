a8886ff5efd288bf7c76296fb28edf83623b6a6a
zgylzbvm <2693943158@qq.com>
2024-10-20 01:12:07 +0800

    slove all

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,7 +2,7 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
+
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -30,6 +30,13 @@ impl Graph for UndirectedGraph {
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (from, to, weight) = edge;
+        self.add_node(from);
+        self.add_node(to);
+        let from_neighbors = self.adjacency_table_mutable().get_mut(from).unwrap();
+        from_neighbors.push((to.to_string(), weight));
+        let to_neighbors = self.adjacency_table_mutable().get_mut(to).unwrap();
+        to_neighbors.push((from.to_string(), weight));
     }
 }
 pub trait Graph {
@@ -38,10 +45,23 @@ pub trait Graph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
         //TODO
-		true
+		//true
+        if!self.contains(node) {
+            self.adjacency_table_mutable().insert(node.to_string(), Vec::new());
+            true
+        } else {
+            false
+        }
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (from, to, weight) = edge;
+        self.add_node(from);
+        self.add_node(to);
+        let from_neighbors = self.adjacency_table_mutable().get_mut(from).unwrap();
+        from_neighbors.push((to.to_string(), weight));
+        let to_neighbors = self.adjacency_table_mutable().get_mut(to).unwrap();
+        to_neighbors.push((from.to_string(), weight));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
